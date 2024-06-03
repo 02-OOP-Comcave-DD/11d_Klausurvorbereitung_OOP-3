efd3ddc3b250825e749e93a08792fb277f8611de
Rene <media-r.r@online.de>
2024-06-03 10:55:49 +0200

    Erklärungen erweitert

--- com/cc/java/Child.java
+++ com/cc/java/Child.java
@@ -1,6 +1,6 @@
 package com.cc.java;
 
-public class Child extends Parent{
+public class Child extends Parent{			// extends --> keywort für Vererbung
 
 	public void sayHello() {
 		System.out.println("Hello from Child");
--- com/cc/java/Parent.java
+++ com/cc/java/Parent.java
@@ -1,6 +1,6 @@
 package com.cc.java;
 
-public class Parent extends Grandparent {
+public class Parent extends Grandparent {		// extends --> keywort für Vererbung
 
 	public void sayHello1() {
 		System.out.println("Hello from Parent");
